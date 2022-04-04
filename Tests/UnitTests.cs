89cd600e967f350744440fbe3b105e8b7bd55427
NathanLiew <wliew2@huskers.unl.edu>
2022-04-04 12:33:29 -0500

    Failing Unit Test

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -11,7 +11,8 @@ namespace GithubActionsLab
         {
             Assert.AreEqual(3, Program.Add("1", "2"));
             Assert.AreEqual(5, Program.Add("3", "2"));
-            Assert.AreEqual(11, Program.Add("5", "7"));
+            Assert.AreEqual(12, Program.Add("5", "7"));
+            Assert.AreEqual(4, Program.Add("1", "7"));
         }
 
         [Test]
