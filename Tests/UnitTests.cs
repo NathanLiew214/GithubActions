9915c2d043c7ace5b105dd4d8129191142f05343
NathanLiew <wliew2@huskers.unl.edu>
2022-04-04 12:52:49 -0500

    Implement the other unit tests

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -39,7 +39,6 @@ namespace GithubActionsLab
             Assert.AreEqual(2, Program.Subtract("3", "1"));
             Assert.AreEqual(4, Program.Subtract("6", "2"));
             Assert.AreEqual(3, Program.Subtract("11", "8"));
-
         }
 
         [Test]
